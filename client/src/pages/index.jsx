320cbca70b011b3acec3584ce139a65567fba576
Nikhil Dange <nsdange@ucsd.edu>
2023-10-25 14:07:22 -0700

    starter code

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import styles from '@/styles/Home.module.css'
+import styles from '../styles/Home.module.css'
 
 
 export default function Home() {
@@ -8,11 +8,11 @@ export default function Home() {
     <div className={styles.main}>
         <div className={styles.welcome}>
           <h1> Hello World! </h1>
-          {/* Add a title and short paragraph about your website here! */}
+          {/* Replace the heading, and add a short paragraph about your website here! */}
         </div>
       <div className={styles.image}>
-        {/* Make use of the Next Image component here! */}
-        {/* Keep in mind that any images you want to use must be imported from the /public folder */}
+        {/* Put the ACM logo here! */}
+        {/* Keep in mind that any images you want to use must be imported from the /assets folder */}
       </div>
     </div>
   )
